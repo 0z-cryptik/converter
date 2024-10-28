0688b095c9adb3c4d67c3f666743ba9a58080a53
Eniitan <enitanolawale0@gmail.com>
2024-10-29 00:36:05 +0100

    minor change for responsiveness

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <h1 className="text-3xl absolute top-[40px]">
+    <h1 className="text-3xl absolute top-[40px] text-center">
       Unit converter, built by{" "}
       <a
         href="https://github.com/0z-cryptik"
